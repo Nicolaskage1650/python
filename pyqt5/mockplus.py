a553bcd34965c00e1e2bd0b867c66a6c9b61204b
rrgssagar <rrgs@gmail.com>
2021-10-11 18:41:14 +0530

    sss

--- pyqt5/mockplus.py
+++ pyqt5/mockplus.py
@@ -1,15 +1,20 @@
-from demo import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
-
+from demo import Ui_MainWindow
+ui = None
 def b1_clicked():
    print ("Button 1 clicked")
+   name = ui.input_name.text()
+   print(name)
 
 if __name__ == "__main__":
     import sys
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
+    # class ui start
     ui = Ui_MainWindow()
     ui.setupUi(MainWindow)
     ui.RTX_BUTTON.clicked.connect(b1_clicked)
+    
+    # class ui end
     MainWindow.show()
     sys.exit(app.exec_())
